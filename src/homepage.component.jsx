5e8ccf8131ddcf501585e865cd5031ca35053b34
shivg7706 <shivamgupta7706@gmail.com>
2020-07-17 13:05:49 +0530

    init: initialize project

---
+++ src/homepage.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='tile'>Atta</h1>
+                    <span className='subtitle'>Buy Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='tile'>Chole-Chawal</h1>
+                    <span className='subtitle'>Eat Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='tile'>Vada Pav</h1>
+                    <span className='subtitle'>Taste Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='tile'>Puri Kachori</h1>
+                    <span className='subtitle'>Eat Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage;
