ac7e3ca8c895a712468a529964d2a8cebc07d7c7
shivg7706 <shivamgupta7706@gmail.com>
2020-07-17 15:48:31 +0530

    chore: update component and homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 
-import './homepage.styles.scss';
 
 function App() {
   return (
