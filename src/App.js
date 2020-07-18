835acabcfe0d204c0e318417c2b31f40ff7af740
shivg7706 <shivamgupta7706@gmail.com>
2020-07-18 12:11:56 +0530

    update: add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
 function App() {
   return (
     <div className="App">
-        <Homepage/>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data.js';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            collection: SHOP_DATA
+        }
+
+
+    }
+
+    render() {
+        return(
+            <div>
+                SHOP PAGE
+            </div>
+        )
+    }
+
+
+}
+
+export default ShopPage;
