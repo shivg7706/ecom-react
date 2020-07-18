5f954c5f4dd33e7b592f3f1ddab6fa637c687290
shivg7706 <shivamgupta7706@gmail.com>
2020-07-18 13:25:39 +0530

    feat: collection and preview items

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data.js';
 
+import CollectionPreview from '../../components/preview-collection/preview-collection.component';
+
 class ShopPage extends React.Component {
     constructor(props) {
         super(props);
 
 
         this.state = {
-            collection: SHOP_DATA
+            collections: SHOP_DATA
         }
 
 
     }
 
     render() {
+    const { collections } = this.state;
+
         return(
-            <div>
-                SHOP PAGE
+            <div className='shopPage'>
+                {
+                    collections.map(({ id, ...otherCollectionProps }) =>  (
+                        <CollectionPreview key={id} {...otherCollectionProps}/>
+                    ))
+                }
             </div>
         )
     }
