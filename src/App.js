9d9fdfe78cb1ed021a2517c96ab17799bcb2983c
shivg7706 <shivamgupta7706@gmail.com>
2020-07-18 13:51:08 +0530

    add: header to whole site

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.compoment';
 
 
 function App() {
   return (
     <div className="App">
+      <Header/> 
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/shop' component={ShopPage} />
     </div>
